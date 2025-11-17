7844e6844fb12e2896220e9c063221cb1b1a1ab3
victor <gfeng005@uottawa.ca>
2025-11-16 20:19:12 -0500

    added two simple functions to demonstrate the property binding and event binding

--- Angular-core/src/app/app.component.ts
+++ Angular-core/src/app/app.component.ts
@@ -5,8 +5,16 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Angular-core';
+
+  onLogoClicked() {
+    alert('Hello World');
+  }
+
+  onKeyUp(titleValue: string) {
+    this.title = titleValue;
+  }
 }
