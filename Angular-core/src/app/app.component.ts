2f38c6e7092d310062d3032a0dceeb86e83c1931
victor <gfeng005@uottawa.ca>
2025-12-02 14:07:50 -0500

    added pipe demonstration

--- Angular-core/src/app/app.component.ts
+++ Angular-core/src/app/app.component.ts
@@ -2,15 +2,30 @@ import { Component } from '@angular/core';
 import { CourseCardComponent } from './course-card/course-card.component';
 import { COURSES } from '../db-data';
 import { Course } from '../model/course';
+import {
+  DatePipe,
+  DecimalPipe,
+  SlicePipe,
+  UpperCasePipe,
+} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [CourseCardComponent],
+  imports: [
+    CourseCardComponent,
+    DatePipe,
+    UpperCasePipe,
+    DecimalPipe,
+    SlicePipe,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   courses: Course[] = [...COURSES];
+  startDate = new Date(2000, 0, 1);
+  title = COURSES[0].description;
+  price = 9.9924563789;
 
   onCourseSelected(course: Course) {
     console.log('App component - click event bubbled ...', course);
