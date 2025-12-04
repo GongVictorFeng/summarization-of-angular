1a6070e92818201ee33f517acd3e8a4188f9f170
victor <gfeng005@uottawa.ca>
2025-12-03 21:56:48 -0500

    added demonstration for view children

--- Angular-core/src/app/app.component.ts
+++ Angular-core/src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 import { CourseCardComponent } from './course-card/course-card.component';
 import { COURSES } from '../db-data';
 import { Course } from '../model/course';
@@ -9,15 +16,17 @@ import { Course } from '../model/course';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   courses: Course[] = [...COURSES];
 
-  @ViewChild(CourseCardComponent)
-  card!: CourseCardComponent;
+  @ViewChildren(CourseCardComponent)
+  cards!: QueryList<CourseCardComponent>;
 
-  onCourseSelected(course: Course) {
-    console.log(this.card);
+  ngAfterViewInit(): void {
+    console.log(this.cards);
   }
+
+  onCourseSelected(course: Course) {}
 
   //The first argument of tracking function is index, the second one is the element in the iterable
   trackCourse(index: number, course: Course) {
