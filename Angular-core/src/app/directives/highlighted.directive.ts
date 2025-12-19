c7059f16cb4c79b0160bf92e218a83ad5181e9be
victor <gfeng005@uottawa.ca>
2025-12-19 14:12:01 -0500

    using expression to apply directive to the host element

--- Angular-core/src/app/directives/highlighted.directive.ts
+++ Angular-core/src/app/directives/highlighted.directive.ts
@@ -1,20 +1,23 @@
-import { Directive, HostBinding } from '@angular/core';
+import { Directive, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[highlighted]', // attribute selector
 })
 export class HighlightedDirective {
+  @Input('highlighted')
+  isHighlighted: boolean = false;
+
   constructor() {}
 
   //use HostBinding decorator to write to the className of the DOM Property, the property must be existed
-  @HostBinding('className')
-  get cssClasses() {
-    return 'highlighted'; //return the css classes we want to apply to the host element
-  }
+  // @HostBinding('className')
+  // get cssClasses() {
+  //   return 'highlighted'; //return the css classes we want to apply to the host element
+  // }
 
   //Alternative way
-  // @HostBinding('class.highlighted')
-  // get cssClasses() {
-  //   return true;
-  // }
+  @HostBinding('class.highlighted')
+  get cssClasses() {
+    return this.isHighlighted;
+  }
 }
